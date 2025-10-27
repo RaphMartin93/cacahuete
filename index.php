babb08903a191c701355c20b133435e7735914d7
Raphaël Martin <raph_martin93@hotmail.com>
2025-10-27 16:00:25 +0100

    correction

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ unset($_SESSION['login_error']); // Supprimer le message après affichage
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Secret Santa - Connexion</title>
+    <title>Cacahuète - Connexion</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
